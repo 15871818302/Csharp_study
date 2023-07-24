7d9258130df7dedb9525a4acfb2514403698971b
s.xiong <s.xiong@uniware.com.au>
2023-07-24 14:43:22 +0800

    了解 C# 中的类型定义，以及数据类型转换

--- MyProject/RectAngle.cs
+++ MyProject/RectAngle.cs
@@ -5,17 +5,27 @@ namespace RectAngleApplication {
     // 成员变量
     double length;
     double width;
+    object obj;
+    dynamic d = 20;
+    string str = @"xxxxx"; // @可将一些转义字符看作是普通字符
+    int a = 12;
+    byte c = 10;
     public void AcceptDetails() {
       length = 3.5;
       width = 4.5;
+      obj = 100; // 对象的装箱(值类型转换为对象类型)
     }
     public double GetArea() {
       return length * width;
     }
     public void Display() {
+      double d = 5300.6;
+      int i;
+      i = (int)d;
       Console.WriteLine("Length: {0}", length);
       Console.WriteLine("Width: {0}", width);
       Console.WriteLine("Area: {0}", GetArea());
+      Console.WriteLine(i);
     }
   }
 
